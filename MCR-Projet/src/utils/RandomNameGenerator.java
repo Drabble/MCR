3f19a4cf60e0bd5f0caa63745ae04eff643c9795
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2016-06-12 17:59:06 +0200

    modify names generated

--- MCR-Projet/src/utils/RandomNameGenerator.java
+++ MCR-Projet/src/utils/RandomNameGenerator.java
@@ -3,30 +3,25 @@ package utils;
 import java.util.Random;
 
 /**
- * Created by guillaume on 07.06.16.
+ * Class used to randomly generate enemys names, the names used are taken from the  medieval fantasy television
+ * series Game of Thrones.
  */
-
-
 public class RandomNameGenerator {
-    private static String[] Beginning = { "Kr", "Ca", "Ra", "Mrok", "Cru",
-            "Ray", "Bre", "Zed", "Drak", "Mor", "Jag", "Mer", "Jar", "Mjol",
-            "Zork", "Mad", "Cry", "Zur", "Creo", "Azak", "Azur", "Rei", "Cro",
-            "Mar", "Luk" };
+    private static String[] firstName = { "Tyrion ", "Petyr ", "Jorah ", "Sandor ", "Joffrey ",
+            "Viserys ", "Jeor ", "Jaqen ", "Davos ", "Ramsay ", "Daario ", "Obara ", "Hizdahr ", "Gregor ",
+            "Brynden ", "Areo ", "Septa ", "Tormund ","Lancel ", "Janos " };
 
-    private static String[] Middle = { "air", "ir", "mi", "sor", "mee", "clo",
-            "red", "cra", "ark", "arc", "miri", "lori", "cres", "mur", "zer",
-            "marac", "zoir", "slamar", "salmar", "urak" };
 
-    private static String[] End = { "d", "ed", "ark", "arc", "es", "er", "der",
-            "tron", "med", "ure", "zur", "cred", "mur" };
+    private static String[] lastName = { "Septa ", "Tully", "Hotah ", "Unella", "Sand", "Martell", "Arryn",
+            "Payne", "Reed", "Tyrell", "Clegane", "Bolton", "Lannister", "Mormont", "H'ghar", "Naharis" ,"Baelish","Targaryen"
+            ,"Cassel", "Cleftjaw" };
 
     private static Random rand = new Random();
 
     public static String generateName() {
 
-        return Beginning[rand.nextInt(Beginning.length)] +
-                Middle[rand.nextInt(Middle.length)]+
-                End[rand.nextInt(End.length)];
+        return firstName[rand.nextInt(firstName.length)] +
+                lastName[rand.nextInt(lastName.length)];
 
     }
 
